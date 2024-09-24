3798340ec54b75c02f98891bbf29535605d5a23f
txu22 <tianyi.xu2@mail.mcgill.ca>
2024-09-23 20:51:02 -0400

    middleware implementation starting

---
+++ Milestone_1/RMI_Implementation/Server/Server/Middleware/Middleware.java
@@ -0,0 +1,20 @@
+package Server.Middleware;
+
+import Server.Interface.*;
+import Server.Common.*;
+import Server.Middleware.ServerConfig;
+import java.util.Vector;
+import java.rmi.RemoteException;
+import java.rmi.ConnectException;
+
+import java.rmi.NotBoundException;
+import java.util.*;
+
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Middleware {
+    
+}
