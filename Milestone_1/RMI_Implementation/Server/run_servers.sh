a759e12d89d2a59d0beca08c801266cdfa9e8b92
KihyeokK <joe.brighter20@gmail.com>
2024-09-26 20:41:44 -0400

    specify 4 trottier machines to use for servers and middleware

--- Milestone_1/RMI_Implementation/Server/run_servers.sh
+++ Milestone_1/RMI_Implementation/Server/run_servers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 #TODO: SPECIFY THE HOSTNAMES OF 4 CS MACHINES (tr-open-01, tr-open-02, etc...)
-MACHINES=()
+MACHINES=(tr-open-08 tr-open-09 tr-open-10 tr-open-11)
 
 tmux new-session \; \
 	split-window -h \; \
