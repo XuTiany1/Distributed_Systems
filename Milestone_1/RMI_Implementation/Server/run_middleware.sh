48b30a20186edfe688123f605ed36e0bf572cb1f
KihyeokK <joe.brighter20@gmail.com>
2024-09-26 21:07:38 -0400

    modify run_middleware.sh for usage guide and actually run middleware

--- Milestone_1/RMI_Implementation/Server/run_middleware.sh
+++ Milestone_1/RMI_Implementation/Server/run_middleware.sh
@@ -1,8 +1,9 @@
 ./run_rmi.sh > /dev/null
 
-echo "Edit file run_middleware.sh to include instructions for launching the middleware"
+echo "Usage: ./run_middleware.sh <Flights Server hostname> <Cars Server hostname> <Rooms Server hostname>"
+echo "e.g. ./run_middleware.sh tr-open-08 tr-open-09 tr-open-10"
 echo '  $1 - hostname of Flights'
 echo '  $2 - hostname of Cars'
 echo '  $3 - hostname of Rooms'
 
-# java -Djava.rmi.server.codebase=file:$(pwd)/ Server.RMI.RMIMiddleware $1 $2 $3
+java -Djava.rmi.server.codebase=file:$(pwd)/ Server.RMI.RMIMiddleware $1 $2 $3
