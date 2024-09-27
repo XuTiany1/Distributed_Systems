e9c27504daf7e7b6b18ab044948533a4fca24f8c
KihyeokK <joe.brighter20@gmail.com>
2024-09-26 20:42:18 -0400

    override ResourceManager functions in Middleware

--- Milestone_1/RMI_Implementation/Server/Server/Middleware/Middleware.java
+++ Milestone_1/RMI_Implementation/Server/Server/Middleware/Middleware.java
@@ -10,4 +10,132 @@ public abstract class Middleware extends ResourceManager {
     public Middleware(String name) {
         super(name);
     }
+
+	@Override
+	public boolean addFlight(int flightNum, int flightSeats, int flightPrice) throws RemoteException
+	{
+		return flight_resourceManager.addFlight(flightNum, flightSeats, flightPrice);
+	}
+
+	@Override
+	public boolean addCars(String location, int numCars, int price) throws RemoteException
+	{
+		return car_resourceManager.addCars(location, numCars, price);
+	}
+
+	@Override
+	public boolean addRooms(String location, int numRooms, int price) throws RemoteException
+	{
+		return room_resourceManager.addRooms(location, numRooms, price);
+	}
+
+	@Override
+	public int newCustomer() throws RemoteException
+	{
+		return flight_resourceManager.newCustomer();
+	}
+
+	@Override
+	public boolean newCustomer(int cid) throws RemoteException
+	{
+		return flight_resourceManager.newCustomer(cid);
+	}
+
+	@Override
+	public boolean deleteFlight(int flightNum) throws RemoteException
+	{
+		return flight_resourceManager.deleteFlight(flightNum);
+	}
+
+	@Override
+	public boolean deleteCars(String location) throws RemoteException
+	{
+		return car_resourceManager.deleteCars(location);
+	}
+
+	@Override
+	public boolean deleteRooms(String location) throws RemoteException
+	{
+		return room_resourceManager.deleteRooms(location);
+	}
+
+	@Override
+	public int queryFlight(int flightNum) throws RemoteException
+	{
+		return flight_resourceManager.queryFlight(flightNum);
+	}
+
+	@Override
+	public int queryCars(String location) throws RemoteException
+	{
+		return car_resourceManager.queryCars(location);
+	}
+	
+	@Override
+	public int queryRooms(String location) throws RemoteException
+	{
+		return room_resourceManager.queryRooms(location);
+	}
+
+	@Override
+	public int queryFlightPrice(int flightNum) throws RemoteException
+	{
+		return flight_resourceManager.queryFlightPrice(flightNum);
+	}
+
+	@Override
+	public boolean deleteRooms(String location) throws RemoteException {
+		return room_resourceManager.deleteRooms(location);
+	}
+
+	@Override
+	public int queryFlight(int flightNum) throws RemoteException {
+		return flight_resourceManager.queryFlight(flightNum);
+	}
+
+	@Override
+	public int queryCars(String location) throws RemoteException {
+		return car_resourceManager.queryCars(location);
+	}
+
+	@Override
+	public int queryRooms(String location) throws RemoteException {
+		return room_resourceManager.queryRooms(location);
+	}
+
+	@Override
+	public int queryFlightPrice(int flightNum) throws RemoteException {
+		return flight_resourceManager.queryFlightPrice(flightNum);
+	}
+
+	@Override
+	public int queryCarsPrice(String location) throws RemoteException {
+		return car_resourceManager.queryCarsPrice(location);
+	}
+
+	@Override
+	public int queryRoomsPrice(String location) throws RemoteException {
+		return room_resourceManager.queryRoomsPrice(location);
+	}
+
+	@Override
+	public boolean reserveFlight(int customerID, int flightNum) throws RemoteException {
+		return flight_resourceManager.reserveFlight(customerID, flightNum);
+	}
+
+	@Override
+	public boolean reserveCar(int customerID, String location) throws RemoteException {
+		return car_resourceManager.reserveCar(customerID, location);
+	}
+
+	@Override
+	public boolean reserveRoom(int customerID, String location) throws RemoteException {
+		return room_resourceManager.reserveRoom(customerID, location);
+	}
+
+	@Override
+	public boolean bundle(int customerID, Vector<String> flightNumbers, String location, boolean car, boolean room) throws RemoteException {
+		return false;
+	}
+    
 }
