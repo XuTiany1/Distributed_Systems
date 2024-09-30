22a2b636ba14911f9b4f79effc79b2da79382145
KihyeokK <joe.brighter20@gmail.com>
2024-09-30 10:33:30 -0400

    m1 - add TCPClient.java

---
+++ Milestone_1/TCP/Client/Client/TCPClient.java
@@ -0,0 +1,47 @@
+package Client;
+
+import Server.Common.*;
+import Server.Interface.*;
+
+import TCPHandlers.*;
+
+public class TCPClient extends Client {
+    private static String serverHost = "localhost";
+
+	private static int serverPort = 4019;
+
+	public static void main(String args[])
+	{
+		if (args.length > 0)
+		{
+			serverHost = args[0];
+		}
+
+		try {
+            TCPClient client = new TCPClient();
+			client.connectServer();
+		} 
+		catch (Exception e) {    
+			System.err.println((char)27 + "[31;1mClient exception: " + (char)27 + "[0mUncaught exception");
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+
+	public TCPClient()
+	{
+		super();
+	}
+
+	public void connectServer()
+	{
+        // Open connection to server, by creating a new socket.
+        // We will create new socket by creating TCPClientHandler object.
+        try {
+            this.setTcpClientHandler(new TCPClientHandler(serverHost));
+        } catch (Exception e) {
+            System.err.println("Error from TCPClient.connectServer: could not connect to server");
+            e.printStackTrace();
+        }
+	}
+}
